3b0da278b204c63cd823fa1632bc17d5b20be14d
Jared Rethman <jaredrethman@gmail.com>
2024-02-10 15:14:37 -0600

    Initial install script
    Currently handles wp filesystem download and manipulate

---
+++ cli/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source cli/utils.sh
+
+NGINX_CONFIG_DIR="./config/nginx/sites"
+TEMPLATE_FILE="./config/nginx/nginx-site.conf.template"
+
+nginx_site_conf() {
+    envsubst '${DOMAIN_NAME}' <"$TEMPLATE_FILE" >"${NGINX_CONFIG_DIR}/${DOMAIN_NAME}.conf"
+}
+
+install_wordpress_files() {
+    sh "${ROOT_PATH}/cli/install-wp-fs.sh"
+}
+
+generate_certs() {
+    sh "${ROOT_PATH}/cli/certs.sh"
+}
+
+# Main function
+main() {
+    echo "Script: ./cli/install.sh"
+    # each_site_env nginx_site_conf
+    each_site_env "install_wordpress_files"
+}
+
+# Execute main function
+main
+
+exit 0
